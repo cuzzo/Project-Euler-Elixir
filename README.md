a947c3c306c38008665fee980c9354d643e62beb
Cuzzo <yahn007@outlook.com>
2016-10-28 15:22:44 -0700

    Implement problems 2 and 3.

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # ProjectEuler
 
-**TODO: Add description**
+Answers for [Project Euler](https://projecteuler.net).
 
-## Installation
+## Running
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add `project_euler` to your list of dependencies in `mix.exs`:
-
-    ```elixir
-    def deps do
-      [{:project_euler, "~> 0.1.0"}]
-    end
-    ```
-
-  2. Ensure `project_euler` is started before your application:
-
-    ```elixir
-    def application do
-      [applications: [:project_euler]]
-    end
-    ```
-
+```bash
+mix escript.build
+./project_euler N
+```
--- lib/project_euler.ex
+++ lib/project_euler.ex
@@ -2,7 +2,7 @@ defmodule ProjectEuler do
   def main(args) do
     args
       |> parse_args
-      |> run
+      |> Problem3.run
       |> IO.puts
   end
 
@@ -10,8 +10,36 @@ defmodule ProjectEuler do
     {_, [str], _} = OptionParser.parse(args)
     str
   end
+end
 
-  defp run(str) do
+defmodule Problem3 do
+  def run(str) do
+    s = 600851475143
+    2..round(:math.sqrt(s))
+      |> Enum.filter(fn(x) -> rem(s, x) === 0 end)
+      |> Enum.map(fn(x) -> [x, round(s / x)] end)
+      |> List.flatten
+      |> Enum.sort
+      |> Enum.filter(fn(x) -> Enum.filter(2..round(:math.sqrt(x)), fn(y) -> rem(x, y) == 0 end) |> Enum.count == 0 end)
+      |> List.last
+  end
+end
+
+defmodule Problem2 do
+  defp fib(a, _, 0) do a end
+  defp fib(a, b, n) do fib(b, a+b, n-1) end
+
+  def run(str) do
+    (0..32
+      |> Enum.map(fn(x) -> fib(1, 1, x) end)
+      |> Enum.filter(fn(x) -> rem(x, 2) == 0 end)
+      |> Enum.sum
+    )
+  end
+end
+
+defmodule Problem1 do
+  def run(str) do
      0..999
        |> Enum.filter(fn(x) -> rem(x, 3) == 0 or rem(x, 5) == 0 end)
        |> Enum.reduce(fn(x, acc) -> acc + x end)
